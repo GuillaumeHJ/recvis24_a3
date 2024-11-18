c1b87a72ef8109830fbc69523ee32c3a53b6ddcb
Guillaume Henon-Just <guillaume.henonjust@gmail.com>
2024-11-18 10:09:13 +0100

    no normalization in the data_transform

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ data_transforms = transforms.Compose(
         transforms.RandomResizedCrop(size=(224, 224), antialias=True),
         transforms.ColorJitter(),
         transforms.RandomHorizontalFlip(p=0.5),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 )
 
