8362be8619b27700fb084aa3240d22cbe8b9500b
Guillaume Henon-Just <guillaume.henonjust@gmail.com>
2024-11-18 10:14:36 +0100

    changed clamp line

--- data.py
+++ data.py
@@ -4,10 +4,10 @@ import torch as torch
 data_transforms = transforms.Compose(
     [
         transforms.ToTensor(),
-        transforms.Lambda(lambda x: torch.clamp(x, 0, 1)),  # Clamp values to [0, 1]
         transforms.RandomResizedCrop(size=(224, 224), antialias=True),
         transforms.ColorJitter(),
         transforms.RandomHorizontalFlip(p=0.5),
+        transforms.Lambda(lambda x: torch.clamp(x, 0, 1)),  # Clamp values to [0, 1]
         # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 )
