c9e4dd973e9e03c64fabcdc8d9be9a64bf7430fc
Guillaume Henon-Just <guillaume.henonjust@gmail.com>
2024-11-20 17:12:00 +0100

    change resize

--- data.py
+++ data.py
@@ -14,7 +14,7 @@ import torch as torch
 
 data_transforms = transforms.Compose(
     [
-        transforms.Resize((64, 64)),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
